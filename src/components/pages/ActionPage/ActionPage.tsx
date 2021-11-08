f87a63bb4d0c19587ded76fb7f253fd7fe530558
Joseph Henry <josephenry@protonmail.com>
2021-11-08 12:25:10 +0100

    fix: sort steps by index

--- src/components/pages/ActionPage/ActionPage.tsx
+++ src/components/pages/ActionPage/ActionPage.tsx
@@ -133,9 +133,12 @@ const ActionPage = (): JSX.Element | null => {
         </Box>
 
         <List>
-          {Object.values(action.steps).map(
-            (step) => !step.hide && <StepItem key={step.uuid} step={step} />
-          )}
+          {Object.values(action.steps)
+            .filter((s) => !s.hide)
+            .sort((a, b) => a.index - b.index)
+            .map((step) => (
+              <StepItem key={step.uuid} step={step} />
+            ))}
         </List>
 
         <Fade in={someStepsAreWaitingForInput(action)}>
