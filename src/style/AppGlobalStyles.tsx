32528846f1a0867e382634f548a32203a07135ba
Joseph Henry <josephenry@protonmail.com>
2022-02-03 23:00:40 +0100

    refactor: asterisks are not necessary

--- src/style/AppGlobalStyles.tsx
+++ src/style/AppGlobalStyles.tsx
@@ -7,19 +7,19 @@ const AppGlobalStyles = (): JSX.Element => (
         minHeight: "100vh",
       },
       /* Customizing the scroll bar on Electron */
-      "*::-webkit-scrollbar": {
+      "::-webkit-scrollbar": {
         width: "8px",
         height: "8px",
       },
-      "*::-webkit-scrollbar-track": {
+      "::-webkit-scrollbar-track": {
         backgroundColor: "#323131",
       },
-      "*::-webkit-scrollbar-thumb": {
+      "::-webkit-scrollbar-thumb": {
         backgroundColor: "#545353" /* color of the scroll thumb */,
         borderRadius: "4px" /* roundness of the scroll thumb */,
         border: "2px solid #323131" /* creates padding around scroll thumb */,
       },
-      "*::-webkit-scrollbar-thumb:hover": {
+      "::-webkit-scrollbar-thumb:hover": {
         backgroundColor: "#666565",
       },
     }}
