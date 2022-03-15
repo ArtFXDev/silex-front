17a3cf9d3888f0ab6cf034f1b88ca1853e7ce328
Simon Lambin <slambin@artfx.fr>
2022-03-15 18:13:37 +0100

    feat: add the system processes in the job page

--- src/components/pages/RunningJobsPage/RunningJobsPage.tsx
+++ src/components/pages/RunningJobsPage/RunningJobsPage.tsx
@@ -1,4 +1,6 @@
 import {
+  CircularProgress,
+  Divider,
   Paper,
   Table,
   TableBody,
@@ -8,14 +10,44 @@ import {
   TableRow,
   Typography,
 } from "@mui/material";
+import axios from "axios";
 import { useBlade } from "context/BladeContext";
+import { useEffect, useState } from "react";
 
 import PageWrapper from "../PageWrapper/PageWrapper";
 import KillJobsButton from "./KillJobsButton";
 import ProcessRow from "./ProcessRow";
+import { SystemProcess, SystemProcessRow } from "./SystemProcessRow";
+
+const ProcessFilter = ["vray", "kick", "maya", "houdini", "python"];
 
 const RunningJobsPage = (): JSX.Element => {
   const { bladeStatus } = useBlade();
+  const [systemProcesses, setSystemProcesses] = useState<SystemProcess[]>([]);
+  const [loadingSystemProcesses, setLoadingSystemProcesses] =
+    useState<boolean>();
+
+  const fetchSystemProcesses = () => {
+    setLoadingSystemProcesses(true);
+    const systemProcessesURL = `http://localhost:5118/desktop/processes`;
+    axios.get<SystemProcess[]>(systemProcessesURL).then((response) => {
+      setSystemProcesses(
+        response.data.filter((systemProcess) => {
+          return (
+            ProcessFilter.includes(systemProcess.name.toLowerCase()) &&
+            systemProcess.cpu > 20
+          );
+        })
+      );
+      setLoadingSystemProcesses(false);
+    });
+  };
+
+  useEffect(() => {
+    fetchSystemProcesses();
+    const interval = setInterval(fetchSystemProcesses, 5000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <PageWrapper goBack title="Tasks running on this machine">
@@ -50,6 +82,44 @@ const RunningJobsPage = (): JSX.Element => {
         ) : (
           <Typography color="text.disabled">
             You don{"'"}t have any jobs running on your computer...
+          </Typography>
+        )}
+        <Divider sx={{ m: 3 }} />
+        {systemProcesses && systemProcesses.length > 0 ? (
+          <>
+            <TableContainer component={Paper}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Name</TableCell>
+                    <TableCell>PID</TableCell>
+                    <TableCell>CPU Usage</TableCell>
+                    <TableCell align="center">Kill</TableCell>
+                  </TableRow>
+                </TableHead>
+
+                <TableBody>
+                  {systemProcesses.map((p) => (
+                    <SystemProcessRow
+                      key={p.pid}
+                      p={p}
+                      killCallback={fetchSystemProcesses}
+                    />
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </>
+        ) : loadingSystemProcesses ? (
+          <Typography color="text.disabled">
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <CircularProgress color="info" sx={{ mr: 2 }} size={20} />
+              Fetching system processes...
+            </div>
+          </Typography>
+        ) : (
+          <Typography color="text.disabled">
+            You don{"'"}t have any heavy processes running on your computer...
           </Typography>
         )}
       </div>
---
+++ src/components/pages/RunningJobsPage/SystemProcessRow.tsx
@@ -0,0 +1,65 @@
+import DangerousIcon from "@mui/icons-material/Dangerous";
+import { IconButton, TableCell, TableRow } from "@mui/material";
+import axios from "axios";
+import { useSnackbar } from "notistack";
+
+export interface SystemProcess {
+  name: string;
+  pid: number;
+  cpu: number;
+  mem: {
+    private: number;
+    virtual: number;
+    usage: number;
+  };
+}
+
+export const SystemProcessRow = ({
+  p,
+  killCallback,
+}: {
+  p: SystemProcess;
+  killCallback: () => void;
+}): JSX.Element => {
+  const { enqueueSnackbar } = useSnackbar();
+
+  const handleClick = (pid: number) => {
+    enqueueSnackbar("Kill active process request sent", {
+      variant: "info",
+    });
+    const killProcessesURL = `http://localhost:5119/kill/${pid}`;
+    axios
+      .post(killProcessesURL)
+      .then((response) => {
+        console.log(response);
+        killCallback();
+        enqueueSnackbar("Process successfully killed", {
+          variant: "success",
+        });
+      })
+      .catch(() => {
+        enqueueSnackbar("Error occured while killing the process", {
+          variant: "error",
+        });
+      });
+  };
+
+  return (
+    <>
+      <TableRow key={p.pid}>
+        <TableCell>{p.name}</TableCell>
+        <TableCell>{p.pid}</TableCell>
+        <TableCell>{p.cpu}</TableCell>
+        <TableCell align="center">
+          <IconButton
+            onClick={() => handleClick(p.pid)}
+            aria-label="kill"
+            color="error"
+          >
+            <DangerousIcon />
+          </IconButton>
+        </TableCell>
+      </TableRow>
+    </>
+  );
+};
