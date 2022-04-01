55c785a8e0cd9175fb02c60fb3b1563b23364b5e
Joseph Henry <josephenry@protonmail.com>
2022-04-01 03:10:56 +0200

    feat: silex coins animation p5

---
+++ src/context/AnimationContext.tsx
@@ -0,0 +1,48 @@
+import { Box, Fade } from "@mui/material";
+import SilexCoinsAnimation from "components/common/animations/SilexCoinsAnimation";
+import React, { useContext, useState } from "react";
+
+export interface AnimationContext {
+  triggerSilexCoinsAnimation: (nCoins: number) => void;
+}
+
+export const AnimationContext = React.createContext<AnimationContext>(
+  {} as AnimationContext
+);
+
+interface ProvideAnimationProps {
+  children: JSX.Element;
+}
+
+/**
+ * The ProvideAnimation is used to display animations on top of the page
+ */
+export const ProvideAnimation = ({
+  children,
+}: ProvideAnimationProps): JSX.Element => {
+  const [silexCoins, setSilexCoins] = useState<{ on: boolean; nCoins: number }>(
+    { on: false, nCoins: 0 }
+  );
+
+  const triggerSilexCoinsAnimation = (nCoins: number) => {
+    setSilexCoins({ on: true, nCoins });
+    setTimeout(() => setSilexCoins((old) => ({ ...old, on: false })), 10000);
+  };
+
+  return (
+    <AnimationContext.Provider value={{ triggerSilexCoinsAnimation }}>
+      <>
+        {children}
+
+        <Fade in={silexCoins.on} unmountOnExit timeout={1000}>
+          <Box>
+            <SilexCoinsAnimation nCoins={silexCoins.nCoins} />
+          </Box>
+        </Fade>
+      </>
+    </AnimationContext.Provider>
+  );
+};
+
+export const useAnimation = (): AnimationContext =>
+  useContext(AnimationContext);
--- src/types/modules/fonts.d.ts
+++ src/types/modules/fonts.d.ts
@@ -4,3 +4,5 @@
  */
 declare module "*.woff";
 declare module "*.woff2";
+declare module "*.otf";
+declare module "*.ttf";
