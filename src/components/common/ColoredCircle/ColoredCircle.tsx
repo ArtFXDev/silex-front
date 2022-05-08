0082bac6230abb332a206bb4fdc2622d89d38d88
Joseph Henry <josephenry@protonmail.com>
2022-05-09 00:17:20 +0200

    feat: display game variants badges

--- src/components/common/ColoredCircle/ColoredCircle.tsx
+++ src/components/common/ColoredCircle/ColoredCircle.tsx
@@ -1,9 +1,14 @@
+import { Tooltip } from "@mui/material";
+
 interface ColoredCircleProps {
   size: number;
   color: string;
   marginLeft?: number;
   marginRight?: number;
   border?: string;
+  text?: string;
+  tooltip?: { title: string };
+  style?: React.CSSProperties;
 }
 
 const ColoredCircle = ({
@@ -12,19 +17,40 @@ const ColoredCircle = ({
   marginLeft,
   marginRight,
   border,
-}: ColoredCircleProps): JSX.Element => (
-  <span
-    style={{
-      display: "inline-block",
-      width: size,
-      height: size,
-      borderRadius: "50%",
-      border,
-      marginLeft: marginLeft,
-      marginRight: marginRight,
-      backgroundColor: color,
-    }}
-  />
-);
+  text,
+  tooltip,
+  style,
+}: ColoredCircleProps): JSX.Element => {
+  const content = (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: size,
+        height: size,
+        borderRadius: "50%",
+        border,
+        marginLeft: marginLeft,
+        marginRight: marginRight,
+        backgroundColor: color,
+        lineHeight: 1,
+        ...style,
+      }}
+    >
+      {text || ""}
+    </div>
+  );
+
+  if (tooltip) {
+    return (
+      <Tooltip arrow placement="top" title={tooltip.title}>
+        {content}
+      </Tooltip>
+    );
+  }
+
+  return content;
+};
 
 export default ColoredCircle;
